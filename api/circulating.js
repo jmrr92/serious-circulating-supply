b63006ad98fbe4268c76f46abc543c7247eb41f6
Jesús M. <57804914+jmrr92@users.noreply.github.com>
2026-03-06 11:34:37 +0100

    Update circulating.js

--- api/circulating.js
+++ api/circulating.js
@@ -1,6 +1,6 @@
 const MINT = "H6qnGp5anYgMJYAaPXKSqKVYa6mKDrn1ruAeK5Dmbonk";
 const BUYBACK_WALLET = "D9FXcMojKz4HLtjWVJX25dK2QDE5mt6sPSSd3FXFoAQC";
-const RPC_URL = "https://mainnet.helius-rpc.com/?api-key=93cda123-5b97-4689-a10a-c4f5859a5df4";
+const RPC_URL = `https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`;
 
 async function rpc(id, method, params) {
   const res = await fetch(RPC_URL, {
